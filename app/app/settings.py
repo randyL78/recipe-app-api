3bd17e73d10293c439f2a252e0dfdbcffd341149
Randy Layne <randydavidl78@gmail.com>
2024-07-08 22:41:11 -0400

    Add Swagger for API documentation

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,8 +13,6 @@ import os
 
 from pathlib import Path
 
-from rest_framework.schemas import openapi
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
